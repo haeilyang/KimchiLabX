f2872c97ea50ed7f1e938c47154334b1f79b0583
haeilyang <yth98765@gmail.com>
2025-03-03 21:44:18 +0900

    Fix publicPath for Netlify

--- vue.config.js
+++ vue.config.js
@@ -2,16 +2,6 @@ const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: "docs",  // 🔹 빌드 결과를 docs/ 폴더에 저장
-  publicPath: process.env.NODE_ENV === 'production' ? '/KimchiLabX/' : '/',
-  pluginOptions: {
-    dotenv: true
-  },
-  configureWebpack: {
-    plugins: [
-      new (require('webpack')).DefinePlugin({
-        'process.env.VUE_APP_OPENAI_API_KEY': JSON.stringify(process.env.VUE_APP_OPENAI_API_KEY)
-      })
-    ]
-  }
+  outputDir: "dist",  // Netlify는 dist 폴더를 배포함
+  publicPath: '/',     // Netlify에서는 '/'로 설정해야 정상적으로 동작
 });
